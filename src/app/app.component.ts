8d22e786eb07e1ef66ba782ded2885a4b6608d19
Alexander Murphy <amurphy345@gmail.com>
2016-08-22 10:24:53 -0600

    data coming back from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   moduleId: module.id,
@@ -8,7 +9,14 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
+  items: Observable<any[]>;
+
   constructor(af: AngularFire) {
+    this.items = af.database.list('/chats')  
+
+    this.items.subscribe(x => console.log(x));
+
+    console.log(this.items);
     
   }
   title = 'app works!';
